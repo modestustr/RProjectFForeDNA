ae3e870319b1f0c347d27f5d264bc8a18f1912d6
Uysal YILMAZ <uysaly23@hotmail.com>
2023-04-23 20:48:36 -0500

    read_excel function updated to full arguments

--- RCodes/ImportExcel.R
+++ RCodes/ImportExcel.R
@@ -1,5 +1,13 @@
-ImportExcel <- function(file_path, sheetName, xrange) {
-  result <- read_excel(file_path, sheet = sheetName, range = xrange)
+ImportExcel <- function(file_path, sheetName = NULL, xrange = NULL, col_names = TRUE,
+                        col_types = NULL, na = "", trim_ws = TRUE, skip = 0,
+                        n_max = Inf, guess_max = min(1000, n_max), progress = FALSE,
+                        name_repair = "unique") {
   
-  return (result)
+  result <- read_excel(file_path, sheet = sheetName, range = xrange, col_names = col_names,
+                       col_types = col_types, na = na, trim_ws = trim_ws, skip = skip,
+                       n_max = n_max, guess_max = guess_max, progress = progress,
+                       .name_repair = name_repair)
+  
+  return(result)
 }
+
