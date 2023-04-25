a6d0dd74744b3f030562c0f8db88b5e89d32f04d
Uysal YILMAZ <uysaly23@hotmail.com>
2023-04-25 13:41:03 -0500

    Added param name and descriptions

--- RCodes/ImportExcel.R
+++ RCodes/ImportExcel.R
@@ -1,3 +1,19 @@
+#' Import data from an Excel file
+#'
+#' @param file_path Path to the Excel file
+#' @param sheetName Name of the worksheet to import
+#' @param xrange Range of cells to import, e.g. "A1:B10"
+#' @param col_names Whether to use the first row as column names or not
+#' @param col_types A vector of column types
+#' @param na String representing missing values
+#' @param trim_ws Whether to trim white space from strings
+#' @param skip Number of rows to skip at the beginning
+#' @param n_max Maximum number of rows to read
+#' @param guess_max Maximum number of rows to use for guessing column types
+#' @param progress Whether to display a progress bar or not
+#' @param name_repair How to repair invalid column names
+#'
+#' @return A data frame with the imported data
 ImportExcel <- function(file_path, sheetName = NULL, xrange = NULL, col_names = TRUE,
                         col_types = NULL, na = "", trim_ws = TRUE, skip = 0,
                         n_max = Inf, guess_max = min(1000, n_max), progress = FALSE,
@@ -10,4 +26,3 @@ ImportExcel <- function(file_path, sheetName = NULL, xrange = NULL, col_names = 
   
   return(result)
 }
-
