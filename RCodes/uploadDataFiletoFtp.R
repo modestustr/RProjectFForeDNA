0cf52989792cfd0b38e984064183bc8da8ea6820
Uysal YILMAZ <uysaly23@hotmail.com>
2023-04-30 22:19:30 -0500

    Upload Data File Function Added

---
+++ RCodes/uploadDataFiletoFtp.R
@@ -0,0 +1,25 @@
+if (!require(RCurl))
+  install.packages("RCurl")
+
+library(RCurl)
+
+# FTP server
+ftp_server <- "www.modestusnet.com"
+remote_file <- "httpdocs/Data.xlsx"
+local_file<-"data/Data.xlsx"
+
+isLocalFileExist<- file.exists(local_file)
+
+if(!isLocalFileExist)
+  stop("File does not exist. Execution stopped.")
+
+
+# user and pass
+user <- readline(prompt = "FTP username: ")
+password <- readline(prompt = "FTP password: ")
+
+# FTP connection
+ftp_conn <- ftpUpload(local_file, paste0("ftp://", user, ":", password, "@", ftp_server, "/", remote_file))
+
+# Close Connection
+close(ftp_conn)
