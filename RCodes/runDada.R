e367e7953c487c8f89761b44778ffdb82fc93215
Uysal YILMAZ <uysaly23@hotmail.com>
2023-05-16 22:08:24 -0500

    get trimfile names

--- RCodes/runDada.R
+++ RCodes/runDada.R
@@ -1,14 +1,14 @@
-fnF1<- "data/5_S5_L001_R1_001.fastq.gz"
-fnR1<- "data/5_S5_L001_R2_001.fastq.gz"
+fnF1<- "data/4_S4_L001_R1_001.fastq.gz"
+fnR1<- "data/4_S4_L001_R2_001.fastq.gz"
 
 plotQualityProfile(fnF1)
 plotQualityProfile(fnR1)
 
 
-filterTrimForDada2(fnF1,fnR1, truncLen = c(270,210))
+trimmedFiles<-filterTrimForDada2(fnF1,fnR1, truncLen = c(270,210))
 
-fnF_Trimmed<-"data/5F_4a84c2357d6.fastq.gz"
-fnR_Trimmed<-"data/5R_4a842e441334.fastq.gz"
+fnF_Trimmed<-trimmedFiles[1]
+fnR_Trimmed<-trimmedFiles[2]
 
 plotQualityProfile(fnF_Trimmed)
 plotQualityProfile(fnR_Trimmed)
